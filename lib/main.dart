c69da419bfd72a150eac10497de1963bcaea6afd
Berk Akkaya <berkakkaya000@gmail.com>
2024-11-30 22:09:23 +0300

    theming: add text theme, scaffold bg and filled button theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import "package:bookfinder_app/consts/colors.dart";
 import "package:bookfinder_app/screens/init_screen.dart";
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +17,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
+        textTheme: GoogleFonts.interTextTheme(),
+        scaffoldBackgroundColor: colorBackground,
         progressIndicatorTheme: const ProgressIndicatorThemeData(
           color: Colors.black,
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: const WidgetStatePropertyAll(colorLightBlack),
+            foregroundColor: const WidgetStatePropertyAll(colorBackground),
+            padding: const WidgetStatePropertyAll(EdgeInsets.all(16)),
+            shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            )),
+          ),
         ),
       ),
       home: const InitScreen(),
