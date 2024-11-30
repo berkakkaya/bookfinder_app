cb818253accbd15731bd6bc44fa6f63bb247d30f
Berk Akkaya <berkakkaya000@gmail.com>
2024-11-30 22:23:04 +0300

    feat: add animation options for navigation extension

--- lib/extensions/navigation.dart
+++ lib/extensions/navigation.dart
@@ -1,23 +1,63 @@
+import "package:animations/animations.dart";
 import "package:flutter/material.dart";
 
+enum TransitionAnimationType {
+  plain,
+  sharedAxisX,
+}
+
+PageRouteBuilder<T> _buildSharedAxisXRouteBuilder<T>(Widget screen) {
+  return PageRouteBuilder<T>(
+    pageBuilder: (context, animation, secondaryAnimation) => screen,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return SharedAxisTransition(
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        transitionType: SharedAxisTransitionType.horizontal,
+        child: child,
+      );
+    },
+  );
+}
+
 extension NavigationExt on BuildContext {
-  Future<T?> navigateTo<T>(Widget screen) {
-    return Navigator.of(this).push<T>(
-      MaterialPageRoute(builder: (_) => screen),
-    );
+  Future<T?> navigateTo<T>(
+    Widget screen, {
+    TransitionAnimationType animation = TransitionAnimationType.plain,
+  }) {
+    final Route<T> route = switch (animation) {
+      TransitionAnimationType.sharedAxisX =>
+        _buildSharedAxisXRouteBuilder(screen),
+      _ => MaterialPageRoute(builder: (_) => screen),
+    };
+
+    return Navigator.of(this).push<T>(route);
   }
 
-  Future<T?> navigateToAndRemoveUntil<T>(Widget screen) {
-    return Navigator.of(this).pushAndRemoveUntil<T>(
-      MaterialPageRoute(builder: (_) => screen),
-      (_) => false,
-    );
+  Future<T?> navigateToAndRemoveUntil<T>(
+    Widget screen, {
+    TransitionAnimationType animation = TransitionAnimationType.plain,
+  }) {
+    final Route<T> route = switch (animation) {
+      TransitionAnimationType.sharedAxisX =>
+        _buildSharedAxisXRouteBuilder<T>(screen),
+      _ => MaterialPageRoute(builder: (_) => screen),
+    };
+
+    return Navigator.of(this).pushAndRemoveUntil<T>(route, (_) => false);
   }
 
-  Future<T?> navigateToAndReplace<T, TO>(Widget screen) {
-    return Navigator.of(this).pushReplacement<T, TO>(
-      MaterialPageRoute(builder: (_) => screen),
-    );
+  Future<T?> navigateToAndReplace<T, TO>(
+    Widget screen, {
+    TransitionAnimationType animation = TransitionAnimationType.plain,
+  }) {
+    final Route<T> route = switch (animation) {
+      TransitionAnimationType.sharedAxisX =>
+        _buildSharedAxisXRouteBuilder<T>(screen),
+      _ => MaterialPageRoute(builder: (_) => screen),
+    };
+
+    return Navigator.of(this).pushReplacement<T, TO>(route);
   }
 
   void navigateBack<T extends Object?>([T? result]) {
