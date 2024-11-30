b944b35aab3eccd8f28364186c6f1ab605623457
Berk Akkaya <berkakkaya000@gmail.com>
2024-12-01 01:37:35 +0300

    icons: add icons that are not in flutter's default icons

---
+++ lib/consts/custom_icons.dart
@@ -0,0 +1,26 @@
+// Check README.md for how to update custom icons
+
+import "package:flutter/widgets.dart";
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const String _fontFamily = "CustomIcons";
+
+  static const IconData book5OutlineRounded =
+      IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData book5Rounded =
+      IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData circlesOutlineRounded =
+      IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData circlesRounded =
+      IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData heartCheckRounded =
+      IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData heartPlusOutlineRounded =
+      IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData overviewKeyOutlineRounded =
+      IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData overviewKeyRounded =
+      IconData(0xe907, fontFamily: _fontFamily);
+}
