355b2783d42ba9f601d98bf7f8101e1e4db77d2d
Berk Akkaya <berkakkaya000@gmail.com>
2024-12-05 01:22:21 +0300

    services: add api fns for book search and detail fetching

---
+++ lib/services/api/api_bookdatas_subservice.dart
@@ -0,0 +1,63 @@
+import "package:bookfinder_app/models/api_response.dart";
+import "package:bookfinder_app/services/api/base_api_service.dart";
+import "package:dio/dio.dart";
+
+typedef SearchResults = List<Map<String, dynamic>>;
+typedef BookData = Map<String, dynamic>;
+
+class ApiBookdatasSubservice {
+  Future<ApiResponse<SearchResults>> searchBooks(
+    String query, {
+    required String authHeader,
+  }) async {
+    final dio = BaseApiService.dio;
+
+    try {
+      final response = await dio.get(
+        "/bookSearch",
+        queryParameters: {"q": query},
+        options: Options(headers: {"Authorization": authHeader}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = SearchResults.from(response.data);
+        return ApiResponse(data: data, status: ResponseStatus.ok);
+      }
+
+      return ApiResponse(status: ResponseStatus.unknownError);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 404) {
+        return ApiResponse(status: ResponseStatus.notFound);
+      }
+
+      rethrow;
+    }
+  }
+
+  Future<ApiResponse<BookData>> getBookData(
+    String bookId, {
+    required String authHeader,
+  }) async {
+    final dio = BaseApiService.dio;
+
+    try {
+      final response = await dio.get(
+        "/books/$bookId",
+        options: Options(headers: {"Authorization": authHeader}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = BookData.from(response.data);
+        return ApiResponse(data: data, status: ResponseStatus.ok);
+      }
+
+      return ApiResponse(status: ResponseStatus.unknownError);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 404) {
+        return ApiResponse(status: ResponseStatus.notFound);
+      }
+
+      rethrow;
+    }
+  }
+}
--- lib/services/api/api_recommendations_subservice.dart
+++ lib/services/api/api_recommendations_subservice.dart
@@ -1,13 +1,19 @@
 import "package:bookfinder_app/models/api_response.dart";
 import "package:bookfinder_app/services/api/base_api_service.dart";
+import "package:dio/dio.dart";
 
 typedef RecommendationData = List<Map<String, dynamic>>;
 
 class ApiRecommendationsSubservice {
-  static Future<ApiResponse<RecommendationData>> getRecommendations() async {
+  static Future<ApiResponse<RecommendationData>> getRecommendations({
+    required String authHeader,
+  }) async {
     final dio = BaseApiService.dio;
 
-    final response = await dio.get("/recommendations");
+    final response = await dio.get(
+      "/recommendations",
+      options: Options(headers: {"Authorization": authHeader}),
+    );
 
     if (response.statusCode == 200) {
       final data = RecommendationData.from(response.data);
