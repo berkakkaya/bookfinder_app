a753446b90287877a848f5e2610d74019c75c30a
Berk Akkaya <berkakkaya000@gmail.com>
2024-12-11 00:40:28 +0300

    fix: handle 401 response in `getRecommendations`

--- lib/services/api/api_recommendations_subservice.dart
+++ lib/services/api/api_recommendations_subservice.dart
@@ -10,16 +10,24 @@ class ApiRecommendationsSubservice {
   }) async {
     final dio = BaseApiService.dio;
 
-    final response = await dio.get(
-      "/recommendations",
-      options: Options(headers: {"Authorization": authHeader}),
-    );
+    try {
+      final response = await dio.get(
+        "/recommendations",
+        options: Options(headers: {"Authorization": authHeader}),
+      );
 
-    if (response.statusCode == 200) {
-      final data = RecommendationData.from(response.data);
-      return ApiResponse(data: data, status: ResponseStatus.ok);
+      if (response.statusCode == 200) {
+        final data = RecommendationData.from(response.data);
+        return ApiResponse(data: data, status: ResponseStatus.ok);
+      }
+
+      return ApiResponse(status: ResponseStatus.unknownError);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        return ApiResponse(status: ResponseStatus.unauthorized);
+      }
+
+      rethrow;
     }
-
-    return ApiResponse(status: ResponseStatus.unknownError);
   }
 }
