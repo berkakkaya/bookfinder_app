eda876cf726970b8864d4430d5f4fec8dc90da7a
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-04 21:20:33 +0300

    consts: add mock user datas

---
+++ lib/consts/mock_datas.dart
@@ -0,0 +1,16 @@
+import "package:bookfinder_app/models/user_models.dart";
+
+const List<MockUser> mockUserDatas = [
+  MockUser(
+    userId: "1",
+    nameSurname: "Ahmet Yılmaz",
+    email: "ahmet@gmail.com",
+    password: "ahmet123",
+  ),
+  MockUser(
+    id: "2",
+    name: "Kübra Kaya",
+    email: "kubra@gmail.com",
+    password: "kubra123",
+  ),
+];
