ba21ccea12532c6cbd98e43701ce7dbb07d017ad
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-04 21:19:50 +0300

    exts: add list extension

---
+++ lib/extensions/lists.dart
@@ -0,0 +1,11 @@
+extension ListExt<T> on List<T> {
+  T? firstWhereOrNull(bool Function(T) test) {
+    for (T element in this) {
+      if (test(element)) {
+        return element;
+      }
+    }
+
+    return null;
+  }
+}
