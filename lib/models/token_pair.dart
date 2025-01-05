0e6bf152915ff9848e26c3f55ca39fe8b99b99b7
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-05 15:50:22 +0300

    models: add comparison operators to TokenPair

--- lib/models/token_pair.dart
+++ lib/models/token_pair.dart
@@ -6,4 +6,16 @@ class TokenPair {
     required this.accessToken,
     required this.refreshToken,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is TokenPair &&
+        other.accessToken == accessToken &&
+        other.refreshToken == refreshToken;
+  }
+
+  @override
+  int get hashCode => (accessToken.hashCode + refreshToken.hashCode).hashCode;
 }
