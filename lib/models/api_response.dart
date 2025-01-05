1de1d517b2d8f1053a45f03ae5c5cc73a509ff92
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-05 15:43:59 +0300

    refactor: add docs and make constructor const in ApiResponse

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -10,11 +10,22 @@ enum ResponseStatus {
   unknownError,
 }
 
+/// Represents a response from the API. Response status is represented
+/// by [ResponseStatus] and the data is represented by [data]. By changing
+/// the type of [data], this class can be used to represent different types
+/// of API responses.
+///
+/// Please note that the [data] field can be null. This is because some
+/// API responses may not have any data to return. Consult the API
+/// documentation to determine if the API response will have data.
 class ApiResponse<T> {
+  /// Status of the API response
   final ResponseStatus status;
+
+  /// Data returned by the API
   final T? data;
 
-  ApiResponse({
+  const ApiResponse({
     required this.status,
     this.data,
   });
