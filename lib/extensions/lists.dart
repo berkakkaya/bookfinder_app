95f3d3f9afac245d9c80710ec99e659869cc3919
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-05 17:33:25 +0300

    exts: add function for picking random elements from list

--- lib/extensions/lists.dart
+++ lib/extensions/lists.dart
@@ -1,4 +1,9 @@
+import "dart:math";
+
 extension ListExt<T> on List<T> {
+  /// Returns the first element that satisfies the given predicate [test].
+  ///
+  /// If no element satisfies the predicate, the result will be `null`.
   T? firstWhereOrNull(bool Function(T) test) {
     for (T element in this) {
       if (test(element)) {
@@ -7,5 +12,29 @@ extension ListExt<T> on List<T> {
     }
 
     return null;
+  }
+
+  /// Picks random elements from the list at [count] amount.
+  ///
+  /// If [count] is greater than or equal to the length of the list,
+  /// the list will be shuffled and returned.
+  Iterable<T> pickRandom(
+    int count, {
+    int? seed,
+  }) sync* {
+    final random = Random(seed);
+    final List<T> copy = List<T>.from(this);
+
+    if (count >= length) {
+      copy.shuffle(random);
+
+      yield* copy;
+      return;
+    }
+
+    for (int i = 0; i < count; i++) {
+      final int index = random.nextInt(copy.length);
+      yield copy.removeAt(index);
+    }
   }
 }
