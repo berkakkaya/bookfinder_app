71b7e2541018c368e874d53ea00766c96949a1f8
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-07 00:03:59 +0300

    services: add mock book tracking data list to mock api db

--- lib/services/api/mock_imp/mock_api_db.dart
+++ lib/services/api/mock_imp/mock_api_db.dart
@@ -1,10 +1,12 @@
 import "package:bookfinder_app/consts/mock_datas.dart" as mock_datas;
+import "package:bookfinder_app/models/book_tracking_models.dart";
 import "package:bookfinder_app/models/bookdata_models.dart";
 import "package:bookfinder_app/models/mock_datamodels.dart";
 
 class MockApiDb {
   final List<MockUser> mockUsers = [];
   final List<BookData> mockBookDatas = [];
+  final List<BookTrackingData> mockBookTrackingDatas = [];
 
   MockApiDb({bool fillWithMockDatas = true}) {
     if (fillWithMockDatas) {
