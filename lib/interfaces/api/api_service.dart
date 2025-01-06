9e3063514601ff50262b76a7f2695f77a0236a25
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-07 01:54:22 +0300

    interfaces: add library subservice to ApiService

--- lib/interfaces/api/api_service.dart
+++ lib/interfaces/api/api_service.dart
@@ -1,6 +1,7 @@
 import "package:bookfinder_app/interfaces/api/api_auth_subservice.dart";
 import "package:bookfinder_app/interfaces/api/api_bookdatas_subservice.dart";
 import "package:bookfinder_app/interfaces/api/api_booktracking_subservice.dart";
+import "package:bookfinder_app/interfaces/api/api_library_subservice.dart";
 import "package:bookfinder_app/interfaces/api/api_recommendations_subservice.dart";
 import "package:bookfinder_app/interfaces/api/api_users_subservice.dart";
 
@@ -10,6 +11,7 @@ abstract class ApiService {
   ApiBookdatasSubservice get bookDatas;
   ApiRecommendationsSubservice get recommendations;
   ApiBooktrackingSubservice get bookTracking;
+  ApiLibrarySubservice get library;
 
   Future<bool> checkApiHealth();
 }
