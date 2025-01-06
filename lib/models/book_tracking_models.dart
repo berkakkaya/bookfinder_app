3962e0d9dbf5039117a113e949eb0f3870f7f580
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-06 23:54:46 +0300

    models: add extended book tracking data model to include book details

--- lib/models/book_tracking_models.dart
+++ lib/models/book_tracking_models.dart
@@ -14,3 +14,17 @@ class BookTrackingData {
     required this.status,
   });
 }
+
+class BookTrackingDataWithBookData extends BookTrackingData {
+  final String bookTitle;
+  final List<String> bookAuthors;
+  final String bookThumbnailUrl;
+
+  const BookTrackingDataWithBookData({
+    required super.bookId,
+    required super.status,
+    required this.bookTitle,
+    required this.bookAuthors,
+    required this.bookThumbnailUrl,
+  });
+}
