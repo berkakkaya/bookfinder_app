b7ae38bb1f105ad946e730a3ce538961d104867c
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-10 18:58:59 +0300

    widgets: add onTap parameter and add inkwell animation to BookListCard

--- lib/widgets/book_list_card.dart
+++ lib/widgets/book_list_card.dart
@@ -8,99 +8,105 @@ import "package:flutter/material.dart";
 
 class BookListCard extends StatelessWidget {
   final BookListItem listItem;
+  final void Function() onTap;
 
   const BookListCard({
     super.key,
     required this.listItem,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: colorWhite,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: colorGray,
-          width: 1,
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(16),
+      child: Ink(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: colorWhite,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: colorGray,
+            width: 1,
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 16,
-        children: [
-          Container(
-            width: 64,
-            height: 64,
-            decoration: BoxDecoration(
-              color: pickRandomColor(),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Center(
-              child: Text(
-                listItem.title.leadingLetters.take(2).join(),
-                style: context.theme.textTheme.titleLarge?.copyWith(
-                  color: colorWhite,
-                  fontWeight: FontWeight.bold,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 64,
+              height: 64,
+              decoration: BoxDecoration(
+                color: pickRandomColor(),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  listItem.title.leadingLetters.take(2).join(),
+                  style: context.theme.textTheme.titleLarge?.copyWith(
+                    color: colorWhite,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  listItem.title,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    listItem.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${listItem.bookCount} kitap",
-                      style: context.theme.textTheme.bodyMedium?.copyWith(
-                        color: colorGray,
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "${listItem.bookCount} kitap",
+                        style: context.theme.textTheme.bodyMedium?.copyWith(
+                          color: colorGray,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      "⦁",
-                      style: TextStyle(color: colorGray),
-                    ),
-                    const SizedBox(width: 8),
-                    Icon(
-                      listItem.isPrivate
-                          ? Icons.lock_outline
-                          : Icons.public_rounded,
-                      color: colorGray,
-                      size: 16,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      listItem.isPrivate ? "Gizli" : "Herkese Açık",
-                      style: context.theme.textTheme.bodyMedium?.copyWith(
-                        color: colorGray,
+                      const SizedBox(width: 8),
+                      Text(
+                        "⦁",
+                        style: TextStyle(color: colorGray),
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 8),
+                      Icon(
+                        listItem.isPrivate
+                            ? Icons.lock_outline
+                            : Icons.public_rounded,
+                        color: colorGray,
+                        size: 16,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        listItem.isPrivate ? "Gizli" : "Herkese Açık",
+                        style: context.theme.textTheme.bodyMedium?.copyWith(
+                          color: colorGray,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
