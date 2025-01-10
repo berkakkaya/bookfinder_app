f2aa5d4fae6658f755bdd81c3ee35d93ae137ee5
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-10 17:57:50 +0300

    utils: handle empty strings in parseLeadingLetters function

--- lib/utils/parsing_utils.dart
+++ lib/utils/parsing_utils.dart
@@ -2,7 +2,7 @@ String parseLeadingLetters(
   String input, {
   int? count,
 }) {
-  final leadingLetters = input.split(" ").map((e) => e[0]);
+  final leadingLetters = input.split(" ").map((e) => e.isEmpty ? "" : e[0]);
 
   if (count != null) {
     return leadingLetters.take(count).join();
