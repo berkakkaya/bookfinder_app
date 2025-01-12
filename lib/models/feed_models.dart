5bf1b6370524bd9d1f1ac040750f8e8a8db18102
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-13 02:54:20 +0300

    services/api: implement dio api feed subservice functions

--- lib/models/feed_models.dart
+++ lib/models/feed_models.dart
@@ -8,6 +8,23 @@ class BaseFeedEntry {
     required this.issuerNameSurname,
     required this.issuedAt,
   });
+
+  static BaseFeedEntry fromJson(Map<String, dynamic> json) {
+    final type = json["type"] as String;
+
+    switch (type) {
+      case "book_list_publish":
+        return BookListPublishFeedEntry(
+          issuerUserId: json["issuerUserId"],
+          issuerNameSurname: json["issuerNameSurname"],
+          issuedAt: DateTime.parse(json["issuedAt"]),
+          bookListId: json["bookListId"],
+          bookListName: json["bookListName"],
+        );
+      default:
+        throw ArgumentError("Unknown feed entry type: $type");
+    }
+  }
 }
 
 class BookListPublishFeedEntry extends BaseFeedEntry {
--- lib/services/api/dio_imp/api_feed_subservice.dart
+++ lib/services/api/dio_imp/api_feed_subservice.dart
@@ -1,6 +1,7 @@
 import "package:bookfinder_app/interfaces/api/api_feed_subservice.dart";
 import "package:bookfinder_app/models/api_response.dart";
 import "package:bookfinder_app/models/feed_models.dart";
+import "package:bookfinder_app/utils/convert_utils.dart";
 import "package:dio/dio.dart";
 
 class DioApiFeedSubservice extends ApiFeedSubservice {
@@ -12,8 +13,41 @@ class DioApiFeedSubservice extends ApiFeedSubservice {
   Future<ApiResponse<List<BaseFeedEntry>>> getFeedEntries({
     bool fetchUpdatesFromOthers = false,
     required String authHeader,
-  }) {
-    // TODO: implement getFeedEntries
-    throw UnimplementedError();
+  }) async {
+    try {
+      final response = await _dio.get(
+        "/feed",
+        queryParameters: {
+          "getUpdatesFromOthers": fetchUpdatesFromOthers == true ? "1" : "0",
+        },
+        options: Options(
+          headers: {
+            "Authorization": authHeader,
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final feedEntries = (response.data["feed"] as List)
+            .map((entry) => BaseFeedEntry.fromJson(entry))
+            .toList();
+
+        return ApiResponse(
+          status: ResponseStatus.ok,
+          data: feedEntries,
+        );
+      }
+
+      return ApiResponse(status: ResponseStatus.unknownError);
+    } on DioException catch (e) {
+      final responseType = parseResponseStatus(e.response?.statusCode);
+
+      if (responseType != null) {
+        return ApiResponse(status: responseType);
+      }
+
+      // Uncovered error occurred, rethrow it
+      rethrow;
+    }
   }
 }
